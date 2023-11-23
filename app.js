9147213447b92401eebb50b0475c209f7ad0bb6c
Paul <C21359216@mytudublin.ie>
2023-11-23 18:20:51 +0000

    getting started with API and getting all pages set up

--- app.js
+++ app.js
@@ -3,29 +3,47 @@ var app = express();
 var http = require('http');
 var mysql = require('mysql2');
 var url = require('url');
+var path = require('path');
 
-// Create a MySQL database connection
-var dbConnection = mysql.createConnection({
-  host: 'localhost', // Replace with your database host
-  user: 'Builder', // Replace with your database username
-  password: 'cls4', // Replace with your database password
-  database: 'sys', // Replace with your database name
-});
-
-// Connect to the database
-dbConnection.connect(function (err) {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to the database');
-});
-
-// Serve the HTML file from the same directory
-app.use(express.static(__dirname));
+//// Create a MySQL database connection
+//var dbConnection = mysql.createConnection({
+//  host: 'localhost', // Replace with your database host
+//  user: 'public', // Replace with your database username
+//  password: 'password', // Replace with your database password
+//  database: 'sys', // Replace with your database name
+//});
+//
+//// Connect to the database
+//dbConnection.connect(function (err) {
+//  if (err) {
+//    console.error('Error connecting to the database: ' + err.stack);
+//    return;
+//  }
+//  console.log('Connected to the database');
+//});
+//
+//// Serve the HTML file from the same directory
+//app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'Project Files')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/Project Files/index.html');
+});
+
+app.get('/checkout', (req, res) => {
+  res.sendFile(__dirname + '/Project Files/checkout.html');
+});
+
+app.get('/signin', (req, res) => {
+  res.sendFile(__dirname + '/Project Files/signin.html');
+});
+
+app.get('/album', (req, res) => {
+  res.sendFile(__dirname + '/Project Files/album.html');
+});
+
+app.get('/signup', (req, res) => {
+  res.sendFile(__dirname + '/Project Files/signup.html');
 });
 
 app.listen(8080);
