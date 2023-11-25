c51650fa8117d2f9dee1396f6e56955f9457458e
Paul <C21359216@mytudublin.ie>
2023-11-25 11:16:48 +0000

    Database connection for Paul

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-var mysql = require('mysql2');
+var { Pool } = require('pg');
 var url = require('url');
 var path = require('path');
+app.use(express.json());
 
-//// Create a MySQL database connection
-//var dbConnection = mysql.createConnection({
-//  host: 'localhost', // Replace with your database host
-//  user: 'public', // Replace with your database username
-//  password: 'password', // Replace with your database password
-//  database: 'sys', // Replace with your database name
+// Pauls Connection
+var pool = new Pool({
+  user: 'paul',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'password',
+  port: 54321
+});
+
+//// Williams Connection
+//var pool = new Pool({
+//  user: '', // PostgreSQL database username
+//  host: 'localhost', // PostgreSQL database host
+//  database: '', // PostgreSQL database name
+//  password: '', // PostgreSQL database password
+//  port: , // PostgreSQL database port
 //});
-//
-//// Connect to the database
-//dbConnection.connect(function (err) {
-//  if (err) {
-//    console.error('Error connecting to the database: ' + err.stack);
-//    return;
-//  }
-//  console.log('Connected to the database');
-//});
-//
-//// Serve the HTML file from the same directory
-//app.use(express.static(__dirname));
+
 app.use(express.static(path.join(__dirname, 'Project Files')));
 
 app.get('/', (req, res) => {
@@ -46,4 +46,72 @@ app.get('/signup', (req, res) => {
   res.sendFile(__dirname + '/Project Files/signup.html');
 });
 
-app.listen(8080);
+app.get('/getdata', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM car');
+    const results = { 'results': (result) ? result.rows : null };
+    res.send(results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.post('/delete', async (req, res) => {
+  try {
+    const { id } = req.body;
+    const client = await pool.connect();
+    await client.query('DELETE FROM car WHERE id = $1', [id]);
+    const result = await client.query('SELECT * FROM car');
+    const results = { 'results': (result) ? result.rows : null };
+    res.send({ message: "Remaining results", results: results });
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.post('/add', async (req, res) => {
+  try {
+    const { make, model } = req.body;
+    const client = await pool.connect();
+    const result = await client.query('INSERT INTO car (make, model) VALUES ($1, $2) RETURNING id', [make, model]);
+    const id = result.rows[0].id;
+    const queryResult = await client.query('SELECT * FROM car WHERE id = $1', [id]);
+    const results =(queryResult) ? queryResult.rows : null;
+    res.send({ message: "New Car", results: results });
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error " + err);
+  }
+});
+
+app.post('/update', async (req, res) => {
+  try {
+    const { old_model, new_model } = req.body;
+    const client = await pool.connect();
+    const updateResult = await client.query('UPDATE car SET model = $1 WHERE model = $2 RETURNING id', [new_model, old_model]);
+
+    if (updateResult.rows.length === 0) {
+      res.status(404).send({ message: "No car found for the given old model" });
+      return;
+    }
+
+    const id = updateResult.rows[0].id;
+    const queryResult = await client.query('SELECT * FROM car WHERE id = $1', [id]);
+    const results = (queryResult) ? queryResult.rows : null;
+    res.send({ message: "Updated Car", results: results });
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error " + err);
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
