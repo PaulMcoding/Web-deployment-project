a2a2e5b63b73080a4af6d0e7833f1877126022db
William <williamdmoore2002@gmail.com>
2023-11-28 16:42:29 +0000

    more cars fixed

--- table.sql
+++ table.sql
@@ -26,16 +26,16 @@ insert into car(car_make, car_model, car_price, car_year, car_miles, car_locatio
 values('Nissan', 'Skyline r32', 64555.99, '1994', '101486', 'Dublin', 0, 'https://car-images.bauersecure.com/wp-images/1372/1056x594/skyline_01.jpg');
 
 insert into car(car_make, car_model, car_price, car_year, car_miles, car_location, car_sold, car_image)
-values('Ineos', 'Grenadier', 84500, '2023', '142', 'London', 0, 'https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_1080,q_auto:eco,w_1920/v1/cms/uploads/such2o29cjotnluoe540');
+values('Ineos', 'Grenadier', 84500.00, '2023', '0', 'London', 0, 'https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_1080,q_auto:eco,w_1920/v1/cms/uploads/such2o29cjotnluoe540');
 
 insert into car(car_make, car_model, car_price, car_year, car_miles, car_location, car_sold, car_image)
-values('Porsche', 'GT3RS', , '223800', '2021', '36', 0, 'https://smgmedia.blob.core.windows.net/images/129280/1920/porsche-911-gt3-coupe-10814f193b36.jpg');
+values('Porsche', 'GT3RS', 223800, '2021', '2475', 'Dublin', 0, 'https://smgmedia.blob.core.windows.net/images/129280/1920/porsche-911-gt3-coupe-10814f193b36.jpg');
 
 insert into car(car_make, car_model, car_price, car_year, car_miles, car_location, car_sold, car_image)
-values('Aston Martin', 'DB9', , '84950', '27870', 'Cork', 0, 'https://assets.newatlas.com/dims4/default/30e5a82/2147483647/strip/true/crop/1280x720+0+31/resize/1200x675!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2Faston-martin-db9-2013.jpg');
+values('Aston Martin', 'DB9', 84950.00, '2015', '27870', 'Cork', 0, 'https://assets.newatlas.com/dims4/default/30e5a82/2147483647/strip/true/crop/1280x720+0+31/resize/1200x675!/quality/90/?url=http%3A%2F%2Fnewatlas-brightspot.s3.amazonaws.com%2Farchive%2Faston-martin-db9-2013.jpg');
 
 insert into car(car_make, car_model, car_price, car_year, car_miles, car_location, car_sold, car_image)
-values('Toyota', 'GR Corolla', , '37195', '2022', '47650', 0, 'https://www.topgear.com/sites/default/files/2022/06/1-Toyota-Corolla-GR-Morizo-Edition.jpg');
+values('Toyota', 'GR Corolla', 37195.00, '2022', '47650', 'Wexford', 0, 'https://www.topgear.com/sites/default/files/2022/06/1-Toyota-Corolla-GR-Morizo-Edition.jpg');
 
 insert into car(car_make, car_model, car_price, car_year, car_miles, car_location, car_sold, car_image)
 values('', '', , '', '', '', 0, '');
