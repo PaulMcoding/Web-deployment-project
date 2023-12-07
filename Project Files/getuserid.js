e969f5e616aa164810a63cc15c72cb5b3fea8657
Paul <C21359216@mytudublin.ie>
2023-12-07 16:56:20 +0000

    all files now login logout like

---
+++ Project Files/getuserid.js
@@ -0,0 +1,34 @@
+function getUserID() {
+    console.log('Start fetching User ID...');
+    fetch('/getUserId', {
+      method: 'GET',
+      credentials: 'same-origin',
+    })
+  
+    .then(response => {
+      console.log('Response status:', response.status);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch user ID');
+      }
+    })
+    
+    .then(data => {
+      console.log('User ID data:', data);
+          if (data.userID != null) {
+            document.getElementById('authButton').style.display = 'none';
+            document.getElementById('authButton2').style.display = 'block';
+          } else {
+            document.getElementById('authButton').style.display = 'block';
+            document.getElementById('authButton2').style.display = 'none';
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching user ID:', error);
+        });
+      }
+  
+      window.onload = function() {
+        getUserID();
+      };
